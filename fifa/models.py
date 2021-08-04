881ccc37e72515055426d0e0d65e1a81e5d82527
fabianpsalazar <fabianpsalazar@gmail.com>
2021-08-04 14:56:51 -0500

    First commit

---
+++ fifa/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, AbstractUser, BaseUserManager, PermissionsMixin
+
+# Create your models here.
+class User(AbstractUser):
+    pass 
+
+class Teams(models.Model):
+    team_id = models.AutoField(primary_key=True)
+    page = models.IntegerField()
+    total_pages = models.IntegerField()
+    items = models.IntegerField()
+    total_items = models.IntegerField()
+    
+class Players(models.Model):
+    players_id = models.ForeignKey(Teams, on_delete=models.CASCADE, verbose_name='related players')
+    name = models.CharField(max_length=100) 
+    position = models.CharField(max_length=5)
+    nation = models.models.CharField(max_length=20)
---
+++ fifa/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+# Create your views here.
+from rest_framework import status
+
+
+class Fifa_api(APIView):
+
+    pass
